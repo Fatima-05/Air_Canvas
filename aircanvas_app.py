70a93fa207b4faa5f87cf12d191bce23a59e7433
Fatima-05 <fatima.tu.zahra.005@gmail.com>
2026-03-24 22:21:52 +0500

    changing environment

--- aircanvas_app.py
+++ aircanvas_app.py
@@ -37,7 +37,7 @@ class SoundEngine:
 
 class HandSystem:
     def __init__(self):
-        self.mp_hands=mp.solutions_hands
+        self.mp_hands=mp.solutions.hands
         self.hands=self.mp_hands.Hands(max_num_hands=1,min_detection_confidence=0.7)
 
     def process(self,img):
@@ -53,13 +53,13 @@ class HandSystem:
 class ArcPallete:
     def __init__(self):
         self.colors=[
-            (0,0,255)
-            (0,165,255)
-            (0,255,255)
-            (0,255,0)
-            (255,255,0)
-            (255,0,255)
-            (255,255,255)
+            (0,0,255),
+            (0,165,255),
+            (0,255,255),
+            (0,255,0),
+            (255,255,0),
+            (255,0,255),
+            (255,255,255),
             (0,0,0)
         ]
         self.selected_index=4
